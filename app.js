0b8092a0c9647235dd7b29787cae6a68a304b995
Julio Gabriel <jgpjuliope@gmail.com>
2023-11-21 16:01:19 -0300

    Adicionando dando as missoes suas funçoes / conecção com o banco criada!

--- app.js
+++ app.js
@@ -1,61 +1,89 @@
 const express = require('express');
 const app = express();
+// const { v4: uuidv4 } = require('uuid');
+const pool = require('./connection.js');
 app.use(express.json());
 
-const missions = []; // Armazenamento em memória para as missões
-
 // Rota para criar uma nova missão
-app.post('/missions', (req, res) => { 
-    const { title, description } = req.body;
-    const newMission = { id: Date.now(), title, description };
-    missions.push(newMission);
-    res.status(201).json(newMission);
-});
+app.post('/missao', async (req, res) => {
+    try {
+        const { titulo, descricao } = req.body;
+        const dataAtual = new Date().toISOString();
+        const [rows] = await pool.execute(
+            'INSERT INTO missoes (titulo, descricao, data) VALUES (?, ?, ?)',
+            [titulo, descricao, dataAtual]
+        );
+
+        const novaMissao = {
+            titulo,
+            descricao,
+            data: dataAtual,
+        };
+
+        res.status(201).json(novaMissao);
+    } catch (error) {
+        console.error('Erro ao inserir no banco de dados:', error);
+        res.status(500).send('Erro interno do servidor');
+    }
+    });
 
 // Rota para listar todas as missões
-app.get('/listmissions', (req, res) => {
-    const title = "Title teste";
-    const description = "Description teste"
-    const newMission = { id: Date.now(), title, description };
-    missions.push(newMission);
-    res.status(201).json(newMission);
+app.get('/listaMissao', async (req, res) => {
+    try {
+        const [rows] = await pool.execute('SELECT * FROM missoes');
+        res.json(rows);
+    } catch (error) {
+        console.error('Erro ao recuperar dados do banco de dados:', error);
+        res.status(500).send('Erro interno do servidor');
+    }
 });
 
 // Rota para obter uma missão pelo ID
-app.get('/missions/:id', (req, res) => {
-    const missionId = parseInt(req.params.id);
-    const mission = missions.find((m) => m.id === missionId);
-    if (!mission) {
-        return res.status(404).json({ message: 'Missão não encontrada' });
+app.get('/missao/:id', async (req, res) => {
+    try {
+        const missionId = parseInt(req.params.id);
+        const [rows] = await pool.execute('SELECT * FROM missoes WHERE id=?',[missionId]);
+        res.json(rows)
+    } catch (error) {
+        console.error('Erro ao recuperar dados do banco de dados:', error);
+        res.status(500).send('Erro interno do servidor');
     }
-    res.json(mission);
 });
 
 // Rota para atualizar uma missão pelo ID
-app.put('/attmissions/:id', (req, res) => {
-    const missionId = parseInt(req.params.id);
-    const { title, description } = req.body;
-    const mission = missions.find((m) => m.id === missionId);
-    if (!mission) {
-        return res.status(404).json({ message: 'Missão não encontrada' });
+app.put('/missao/:id', async (req, res) => {
+    try {
+        const missaoId = parseInt(req.params.id);
+        const { titulo, descricao } = req.body;
+        const dataAtual = new Date().toISOString();
+
+        const [rows] = await pool.execute(
+            'UPDATE missoes SET titulo=?, descricao=?, data=? WHERE id=?',
+            [titulo, descricao, dataAtual, missaoId]
+        );
+        res.status(200).send('Atualizado');
+    } catch (error) {
+        console.error('Erro ao atualizar no banco de dados:', error);
+        res.status(500).send('Erro interno do servidor');
     }
-    mission.title = title;
-    mission.description = description;
-    res.json(mission);
 });
 
 // Rota para excluir uma missão pelo ID
-app.delete('/deletemissions/:id', (req, res) => {
-    const missionId = parseInt(req.params.id);
-    const index = missions.findIndex((m) => m.id === missionId);
-    if (index === -1) {
-        return res.status(404).json({ message: 'Missão não encontrada' });
+app.delete('/missao/:id', async (req, res) => {
+    try {
+        const missaoId = parseInt(req.params.id);
+        const [rows] = await pool.execute(
+            'DELETE FROM missoes WHERE id = ?', [missaoId]
+        );
+        
+        res.status(200).send('Missao Deletada')
+    } catch (error) {
+        console.error('Erro ao atualizar no banco de dados:', error);
+        res.status(500).send('Erro interno do servidor');
     }
-    missions.splice(index, 1);
-    res.json({ message: 'Missão excluída com sucesso' });
 });
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`Servidor em execução na porta ${port}`);
+    console.log(`Servidor em execução na porta ${port}`);
 });
