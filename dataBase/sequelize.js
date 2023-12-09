c2727886fdaf52f081ac0aa630b5e2eaeceee36e
Julio Gabriel <jgpjuliope@gmail.com>
2023-12-08 21:49:44 -0300

    tentando fazer upload

--- dataBase/sequelize.js
+++ dataBase/sequelize.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('mydb', 'root', 'root', {
-    host: '127.0.0.1',
-    port: 3306,
-    dialect: 'mysql'
+const sequelize = new Sequelize('mydb_8yjb', 'mydb_8yjb_user', 'XhUAK3u5RjDZrFO3bLoshixuPLgdSi3i', {
+    host: 'dpg-clprc7hjvg7s73f3v2vg-a',
+    port: 5432,
+    dialect: 'postgres'
 });
 
 module.exports = sequelize;
